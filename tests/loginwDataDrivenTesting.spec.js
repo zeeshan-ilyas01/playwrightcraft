02b167cf75263dc150c60031f0d5bf79bde25c4e
zeeshan-ilyas01 <zeeshanilyas242@gmail.com>
2025-04-18 17:47:12 +0500

    datadriventesting commit

---
+++ tests/loginwDataDrivenTesting.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test'
+const testdata = JSON.parse(JSON.stringify(require("../logintestdata.json")))
+
+test.describe("Data driven login Test",function(){
+
+    for(const data of testdata)
+    {
+       // test.describe('login with user ${data.id}',function(){
+        test.describe(`login with user ${data.id}`, function() {
+
+            test('login to application', async ({ page }) => {
+                
+
+                await page.goto("https://freelance-learn-automation.vercel.app/login")
+                await page.locator("//input[@id='email1']").fill(data.username)
+                console.log('testdata'+data.username)
+                await page.locator("//input[@id='password1']").fill(data.password)
+               // await page.locator("//button[@class='submit-btn']").click()
+              // await page.pause()        
+            })  
+        })
+    }
+})
