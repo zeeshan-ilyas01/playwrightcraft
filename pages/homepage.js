7ec38417c870b5ca557fd505509b1701c696bc41
zeeshan-ilyas01 <zeeshanilyas242@gmail.com>
2025-04-19 13:15:23 +0500

    commit POM login & logout fun with page visisbility assertion.

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,20 +1,27 @@
-export default class HomePageClass{
+import { expect } from "@playwright/test"
+export default class HomePageClass {
 
-    constructor(page){
+    constructor(page) {
 
-        this.page=page
-        this.menu="//img[@alt='menu']"
-        this.signout="//button[normalize-space()='Sign out']"
-         ////button[@class="nav-menu-item"] and
-        
+        this.page = page
+        this.menu = "//img[@alt='menu']"
+        this.signout = "//button[normalize-space()='Sign out']"
+        this.manage = "//span[normalize-space()='Manage']"
+       
+       
+        ////button[@class="nav-menu-item"] and
+
     }
 
+    async verifyManageOptions() {
+        await expect(this.page.locator(this.manage)).toBeVisible()
 
+    }
 
-async logoutfromApplication(){
+    async logoutfromApplication() {
 
-    await this.page.click(this.menu)
-    await this.page.click(this.signout)
-}
+        await this.page.click(this.menu)
+        await this.page.click(this.signout)
+    }
 
 }
--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,26 +1,28 @@
 // in this js file we have the locators and methods that will perform actions on patricual page.
-
+import { expect } from "@playwright/test"
 export default class LoginPageclass {
 
     constructor(page) {
 
         this.page = page
-       /* this.page = username.locator(//input[@id="email1"])
-            this.password = locator["//input[@id="password1"]"]
-            this.submit = locator //button[@type="submit"][normalize-space()='Sign in']
-*/
+        /* this.page = username.locator(//input[@id="email1"])
+             this.password = locator["//input[@id="password1"]"]
+             this.submit = locator //button[@type="submit"][normalize-space()='Sign in']
+        */
 
+        this.header = "//h2[contains(text(), 'Sign In')]"
+        this.username = "#email1"
+        this.password = "//input[@placeholder='Enter Password']"
+        this.loginbutton = "//button[text()='Sign in']"
+    }
 
-this.username="#email1"
-this.password="//input[@placeholder='Enter Password']"
-this.loginbutton="//button[text()='Sign in']"
+    async verifySignInHeader() {
 
+        await expect(this.page.locator(this.header)).toBeVisible()
 
     }
 
-
-
-    async loginToAppliatio() {
+    async loginToAppliation() {
 
         await this.page.fill(this.username, "admin@email.com")
         await this.page.fill(this.password, "admin@123")
@@ -28,5 +30,3 @@ this.loginbutton="//button[text()='Sign in']"
     }
 
 }
-
-//module.exports=LoginPage;
